76f35397dd563388bcb48f0ba2e679ab9b120664
coopermcpartland <coopermcpartland@gmail.com>
2024-02-21 14:31:37 +1100

    Updated Decoder to automatically detect shift value

--- caesarCipher.js
+++ caesarCipher.js
@@ -7,7 +7,7 @@ function processMessage() {
     if (operation === "encode") {
         result = caesarCipherEncode(message, shift);
     } else if (operation === "decode") {
-        result = caesarCipherDecode(message, shift);
+        result = caesarCipherDecode(message);
     }
 
     document.getElementById("processed-message").innerText = result;
@@ -28,6 +28,31 @@ function caesarCipherEncode(message, shift) {
     return encodedMessage;
 }
 
-function caesarCipherDecode(message, shift) {
+function caesarCipherDecode(message) {
+    // Automatically detect the shift value for decoding
+    var shift = calculateShiftForDecoding(message);
     return caesarCipherEncode(message, 26 - shift);
+}
+
+function calculateShiftForDecoding(message) {
+    // Calculate the most frequent character's occurrence to determine the shift value
+    var charCount = {};
+    for (var i = 0; i < message.length; i++) {
+        var char = message.charAt(i).toUpperCase();
+        if (char >= 'A' && char <= 'Z') {
+            charCount[char] = (charCount[char] || 0) + 1;
+        }
+    }
+
+    var maxChar = '';
+    var maxCount = 0;
+    for (var char in charCount) {
+        if (charCount[char] > maxCount) {
+            maxCount = charCount[char];
+            maxChar = char;
+        }
+    }
+
+    var shift = (maxChar.charCodeAt(0) - 'E'.charCodeAt(0) + 26) % 26;
+    return shift;
 }
